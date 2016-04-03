66de4e042d75d0c2a6b6304761feae011cfa726b
XAMEUS <xam.master.3d@gmail.com>
2016-04-03 22:02:59 +0200

    drawing text

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
-CFLAGS = -Wall `sdl2-config --cflags` `xml2-config --cflags` `pkg-config --cflags glib-2.0` -lm -lGLU
-LDFLAGS= `sdl2-config --libs` `xml2-config --libs` `pkg-config --libs glib-2.0` -lm -lGLU
+CFLAGS = -Wall `sdl2-config --cflags` `xml2-config --cflags` `pkg-config --cflags glib-2.0`  `freetype-config --cflags` -lm -lGLU -lGLEW
+LDFLAGS= `sdl2-config --libs` `xml2-config --libs` `pkg-config --libs glib-2.0` `freetype-config --libs` -lm -lGLU -lGLEW
 EXEC = main
 HEADERS = $(wildcard *.h)
 OBJECTS = $(patsubst %.c, %.o, $(wildcard *.c))
@@ -13,7 +13,7 @@ all: $(EXEC)
 main: display.o listref.o parse.o draw.o render.o ui.o main.o tessellation.o load.o
 	$(CC) $^ -lGLU -lGL $(LDFLAGS)
 
-drawing: display.o draw.o drawing.o tessellation.o
+drawing: display.o draw.o drawing.o tessellation.o shader_utils.o
 	$(CC) -o drawing $^ -lGLU -lGL $(LDFLAGS)
 
 clean:
@@ -23,4 +23,4 @@ indent:
 	indent -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i8 -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1 *.c -l125
 
 mrproper: clean
-	rm -f $(EXEC)
+	rm -f *.o $(EXEC)
--- drawing.c
+++ drawing.c
@@ -1,237 +1,258 @@
+#include <GL/glew.h>
 #include "draw.h"
 #include "load.h"
 #include <GL/glu.h>
+#include <ft2build.h>
+#include "shader_utils.h"
+
+#include FT_FREETYPE_H
 
 int width = 640;
 int height = 480;
 
 #define size 4
 
-void Display_Render(SDL_Renderer* renderer, int width, int height, float dx, float dy, float zoom);
+int screen_width=800, screen_height=600;
+GLuint program;
+GLint attribute_coord;
+GLint uniform_tex;
+GLint uniform_color;
 
-void Display_Render(SDL_Renderer* renderer, int width, int height, float dx, float dy, float zoom)
-{
-  // Set the background
-  glClearColor(0.9f, 0.9f, 0.9f, 0.0f);
-  // Clear The Screen And The Depth Buffer
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+struct point {
+	GLfloat x;
+	GLfloat y;
+	GLfloat s;
+	GLfloat t;
+};
 
-  GLdouble quad1[size][2] = { {500,100}, {400,200}, {300,350}, {150,400}};
-  //GLdouble quad1[4][2] = { {500,350}, {350,400}, {200,350}, {150,200} };
-  GLdouble *rows1[size] = { quad1[0], quad1[1], quad1[2], quad1[3]};
-  GLdouble **data1 = rows1;
+GLuint vbo;
 
-  GLdouble ux = quad1[1][0] - quad1[0][0];
-  GLdouble uy = quad1[1][1] - quad1[0][1];
-  GLdouble vx = quad1[2][0] - quad1[1][0];
-  GLdouble vy = quad1[2][1] - quad1[1][1];
-  GLdouble wx = quad1[3][0] - quad1[2][0];
-  GLdouble wy = quad1[3][1] - quad1[2][1];
-  GLdouble length = sqrt(ux * ux + uy * uy);
-  ux = ux / length;
-  uy = uy / length;
-  length = sqrt(vx * vx + vy * vy);
-  vx = vx / length;
-  vy = vy / length;
-  printf("u: (%lf, %lf)\n", ux, uy);
-  printf("v: (%lf, %lf)\n", vx, vy);
-  printf("w: (%lf, %lf)\n", wx, wy);
-  printf("%lf\n", atan2(-uy, -ux) - atan2(vy, vx));
-  printf("%lf\n", atan2(-vy, -vx) - atan2(wy, wx));
+FT_Library ft;
+FT_Face face;
 
-  glColor3f(0.9, 0.9, 0.2);
-  glEnable(GL_POLYGON_OFFSET_FILL);
-  glPolygonOffset(1.0, 1.0);
-  Draw_Lines(size, data1, 20);
-  glDisable(GL_POLYGON_OFFSET_FILL);
+const char *fontfilename;
 
-  glColor3f (0.9, 0.1, 0.1);
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-  Draw_Lines(size, data1, 20);
-  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+int Init_Resources() {
+	/* Initialize the FreeType2 library */
+	if (FT_Init_FreeType(&ft)) {
+    fprintf(stderr, "Could not init freetype library\n");
+		return 1;
+	}
 
-  glPopMatrix();
+	/* Load a font */
+	int fontsize;
+	char* font = file_read(fontfilename, &fontsize);
+	if (font == NULL) {
+    fprintf(stderr, "Could not load font file %s\n", fontfilename);
+		return 2;
+	}
+	FT_Error fterr = FT_New_Memory_Face(ft, (FT_Byte*)font, fontsize, 0, &face);
+	if (fterr != FT_Err_Ok) {
+    fprintf(stderr, "Could not init font: error 0x%d\n", fterr);
+		return 3;
+	}
 
-  // Render
-  SDL_RenderPresent(renderer);
+	program = create_program("text.v.glsl", "text.f.glsl");
+
+  printf("prgram: %d\n", program);
+	if(program == 0)
+		return 4;
+
+	attribute_coord = get_attrib(program, "coord");
+	uniform_tex = get_uniform(program, "tex");
+	uniform_color = get_uniform(program, "color");
+
+  printf("attribute_coord: %d\nuniform_tex: %d\nuniform_color: %d\n", attribute_coord, uniform_tex, uniform_color);
+	if(attribute_coord == -1 || uniform_tex == -1 || uniform_color == -1)
+		return 5;
+
+	// Create the vertex buffer object
+	glGenBuffers(1, &vbo);
+
+	return 1;
 }
 
-int main(int argc, char* argv[])
-{
+/**
+ * Render text using the currently loaded font and currently set font size.
+ * Rendering starts at coordinates (x, y), z is always 0.
+ * The pixel coordinates that the FreeType2 library uses are scaled by (sx, sy).
+ */
+void render_text(const char *text, float x, float y, float sx, float sy) {
+	const char *p;
+	FT_GlyphSlot g = face->glyph;
 
-  SDL_Window *window; // Declare a pointer, main window
-  GLfloat dx = 0;
-  GLfloat dy = 0;
-  GLfloat zoom = 1.0;
-  min_x = 0;
-  max_x = width;
-  min_y = 0;
-  max_y = height;
+	/* Create a texture that will be used to hold one "glyph" */
+	GLuint tex;
 
-  // Initialize SDL2
-  if (SDL_Init(SDL_INIT_VIDEO) != 0 )
-  {
-    fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
+	glActiveTexture(GL_TEXTURE0);
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glUniform1i(uniform_tex, 0);
 
-  // Create an application window with the following settings:
-  window = SDL_CreateWindow(
-    "Drawing - OpenGL",            // window title
-    SDL_WINDOWPOS_UNDEFINED,    // initial x position
-    SDL_WINDOWPOS_UNDEFINED,    // initial y position
-    width,                      // width, in pixels
-    height,                     // height, in pixels
-    SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE        // flags
-  );
+	/* We require 1 byte alignment when uploading texture data */
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-  // Check that the window was successfully created
-  if (window == NULL)
-  {
-    // In the case that the window could not be made...
+	/* Clamping to edges is important to prevent artifacts when scaling */
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	/* Linear filtering usually looks best for text */
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	/* Set up the VBO for our vertex data */
+	glEnableVertexAttribArray(attribute_coord);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(attribute_coord, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+	/* Loop through all characters */
+	for (p = text; *p; p++) {
+		/* Try to load and render the character */
+		if (FT_Load_Char(face, *p, FT_LOAD_RENDER))
+			continue;
+
+		/* Upload the "bitmap", which contains an 8-bit grayscale image, as an alpha texture */
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, g->bitmap.width, g->bitmap.rows, 0, GL_ALPHA, GL_UNSIGNED_BYTE, g->bitmap.buffer);
+
+		/* Calculate the vertex and texture coordinates */
+		float x2 = x + g->bitmap_left * sx;
+		float y2 = -y - g->bitmap_top * sy;
+		float w = g->bitmap.width * sx;
+		float h = g->bitmap.rows * sy;
+
+		GLfloat box[4][4] = {
+			{x2, -y2, 0, 0},
+			{x2 + w, -y2, 1, 0},
+			{x2, -y2 - h, 0, 1},
+			{x2 + w, -y2 - h, 1, 1},
+		};
+
+		/* Draw the character on the screen */
+		glBufferData(GL_ARRAY_BUFFER, sizeof box, box, GL_DYNAMIC_DRAW);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+		/* Advance the cursor to the start of the next character */
+		x += (g->advance.x >> 6) * sx;
+		y += (g->advance.y >> 6) * sy;
+	}
+	glDisableVertexAttribArray(attribute_coord);
+	glDeleteTextures(1, &tex);
+}
+
+void render(SDL_Window* window) {
+	float sx = 2.0 / screen_width;
+	float sy = 2.0 / screen_height;
+
+	glUseProgram(program);
+
+	/* White background */
+	glClearColor(1, 1, 1, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	/* Enable blending, necessary for our alpha texture */
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	GLfloat black[4] = { 0, 0, 0, 1 };
+	GLfloat red[4] = { 1, 0, 0, 1 };
+	GLfloat transparent_green[4] = { 0, 1, 0, 0.5 };
+
+	/* Set font size to 48 pixels, color to black */
+	FT_Set_Pixel_Sizes(face, 0, 48);
+	glUniform4fv(uniform_color, 1, black);
+
+	/* Effects of alignment */
+	render_text("The Quick Brown Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 50 * sy, sx, sy);
+	render_text("The Misaligned Fox Jumps Over The Lazy Dog", -1 + 8.5 * sx, 1 - 100.5 * sy, sx, sy);
+
+	/* Scaling the texture versus changing the font size */
+	render_text("The Small Texture Scaled Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 175 * sy, sx * 0.5, sy * 0.5);
+	FT_Set_Pixel_Sizes(face, 0, 24);
+	render_text("The Small Font Sized Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 200 * sy, sx, sy);
+	FT_Set_Pixel_Sizes(face, 0, 48);
+	render_text("The Tiny Texture Scaled Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 235 * sy, sx * 0.25, sy * 0.25);
+	FT_Set_Pixel_Sizes(face, 0, 12);
+	render_text("The Tiny Font Sized Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 250 * sy, sx, sy);
+	FT_Set_Pixel_Sizes(face, 0, 48);
+
+	/* Colors and transparency */
+	render_text("The Solid Black Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 430 * sy, sx, sy);
+
+	glUniform4fv(uniform_color, 1, red);
+	render_text("The Solid Red Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 330 * sy, sx, sy);
+	render_text("The Solid Red Fox Jumps Over The Lazy Dog", -1 + 28 * sx, 1 - 450 * sy, sx, sy);
+
+	glUniform4fv(uniform_color, 1, transparent_green);
+	render_text("The Transparent Green Fox Jumps Over The Lazy Dog", -1 + 8 * sx, 1 - 380 * sy, sx, sy);
+	render_text("The Transparent Green Fox Jumps Over The Lazy Dog", -1 + 18 * sx, 1 - 440 * sy, sx, sy);
+
+	SDL_GL_SwapWindow(window);
+}
+
+void onResize(int width, int height) {
+	screen_width = width;
+	screen_height = height;
+	glViewport(0, 0, screen_width, screen_height);
+
+}
+
+void free_resources() {
+	glDeleteProgram(program);
+}
+
+void mainLoop(SDL_Window* window) {
+	while (1) {
+		SDL_Event ev;
+		while (SDL_PollEvent(&ev)) {
+			if (ev.type == SDL_QUIT)
+				return;
+			if (ev.type == SDL_WINDOWEVENT && ev.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+				onResize(ev.window.data1, ev.window.data2);
+		}
+		render(window);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* window = SDL_CreateWindow("Basic Text",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		screen_width, screen_height,
+		SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+	if (window == NULL) {
     fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
+		return EXIT_FAILURE;
+	}
 
-  SDL_Renderer* displayRenderer = NULL;
-  SDL_RendererInfo displayRendererInfo;
-  displayRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  SDL_GetRendererInfo(displayRenderer, &displayRendererInfo);
-  if ((displayRendererInfo.flags & SDL_RENDERER_ACCELERATED) == 0 ||
-    (displayRendererInfo.flags & SDL_RENDERER_TARGETTEXTURE) == 0) {
-    fprintf(stderr, "We have no render surface and not accelerated.\n");
-  }
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	if (SDL_GL_CreateContext(window) == NULL) {
+		fprintf(stderr, "Error: SDL_GL_CreateContext: %s", SDL_GetError());
+		return EXIT_FAILURE;
+	}
 
-  Display_InitGL();
+	GLenum glew_status = glewInit();
+	if (glew_status != GLEW_OK) {
+		fprintf(stderr, "Error: glewInit: %s", glewGetErrorString(glew_status));
+		return EXIT_FAILURE;
+	}
+	if (!GLEW_VERSION_2_0) {
+		fprintf(stderr, "Error: your graphic card does not support OpenGL 2.0");
+		return EXIT_FAILURE;
+	}
 
-  Display_SetViewport(
-    width, height,
-    dx,
-    dy,
-    zoom
-  );
+	if (argc > 1)
+		fontfilename = argv[1];
+	else
+		fontfilename = "FreeSans.ttf";
 
-  int fullscreen = 0;
-  int drag = 0;
-  int xcursor = 0;
-  int ycursor = 0;
-  SDL_Event event;
-  int quit = 0;
-  time_t t = time(NULL);
-  time_t ctime = time(NULL);
-  int frames = 0;
-  Display_Render(displayRenderer, width, height, dx, dy, zoom);
-  while (!quit)
-  {
-    Display_SetViewport(
-      width, height,
-      dx,
-      dy,
-      zoom
-    );
-    //Display_Render(displayRenderer, width, height, dx, dy, zoom);
-    while (SDL_PollEvent(&event)) // User's actions
-    {
-      switch(event.type)
-      {
-        case SDL_QUIT: // Close button
-          quit = 1;
-          break;
-        case SDL_KEYUP: // Key release
-          if (event.key.keysym.sym == SDLK_f) // F key
-          {
-            if (fullscreen == 0)
-            {
-              fullscreen = 1;
-              SDL_SetWindowFullscreen(window,SDL_WINDOW_FULLSCREEN);
-            }
-            else if (fullscreen == 1)
-            {
-              fullscreen = 0;
-              SDL_SetWindowFullscreen(window, 0);
-            }
-          }
-          if (event.key.keysym.sym == SDLK_KP_PLUS) {
-            if (zoom > 0.1) {
-              dx = dx + 0.1 * pixelsize * (xcursor - width / 2);
-              dy = dy - 0.1 * pixelsize * (ycursor - height / 2);
-            }
-            if (zoom > 0.1)
-              zoom -= 0.1;
-          }
-          if (event.key.keysym.sym == SDLK_KP_MINUS) {
-            dx = dx - 0.1 * pixelsize * (xcursor - width / 2);
-            dy = dy + 0.1 * pixelsize * (ycursor - height / 2);
-            zoom += 0.1;
-          }
-          if (event.key.keysym.sym == SDLK_r)
-          {
-            zoom = 1.0;
-            dx = 0.0;
-            dy = 0.0;
-          }
-          break;
-        case SDL_WINDOWEVENT:
-          switch (event.window.event) {
-            case SDL_WINDOWEVENT_RESIZED:
-              width = event.window.data1;
-              height = event.window.data2;
-              break;
-            case SDL_WINDOWEVENT_SIZE_CHANGED:
-              width = event.window.data1;
-              height = event.window.data2;
-              break;
-            default:
-              // printf("Window %d got unknown event %d\n", event.window.windowID, event.window.event);
-              break;
-          }
-          break;
-        case SDL_MOUSEBUTTONDOWN:
-          if (event.button.button == SDL_BUTTON_LEFT)
-            drag = 1;
-          break;
-        case SDL_MOUSEBUTTONUP:
-          if (event.button.button == SDL_BUTTON_LEFT)
-            drag = 0;
-          break;
-        case SDL_MOUSEMOTION:
-          xcursor = event.motion.x;
-          ycursor = event.motion.y;
-          if (drag != 0) {
-            dx -= pixelsize * event.motion.xrel * zoom;
-            dy += pixelsize * event.motion.yrel * zoom;
-          }
-          break;
-        case SDL_MOUSEWHEEL:
-          if (zoom > 0.03) {
-            dx = dx + 0.03 * pixelsize * event.wheel.y * (xcursor - width / 2);
-            dy = dy - 0.03 * pixelsize * event.wheel.y * (ycursor - height / 2);
-          }
-          if (event.wheel.y > 0 && zoom > 0.01)
-            zoom -= 0.03;
-          else if (event.wheel.y < 0)
-            zoom += 0.03;
-          break;
-        default:
-          //printf("unknown event");
-          break;
-      }
-    }
-    frames++;
-    ctime = time(NULL);
-    if (t != ctime) {
-      t = ctime;
-      fprintf(stdout, "%d fps\n", frames);
-      frames = 0;
-    }
-  }
+	if (!Init_Resources())
+		return EXIT_FAILURE;
 
-  // Close and destroy the window
-  SDL_DestroyWindow(window);
+    mainLoop(window);
 
-  // Clean up
-  SDL_Quit();
-
-  return EXIT_SUCCESS;
+	free_resources();
+	return EXIT_SUCCESS;
 
 }
